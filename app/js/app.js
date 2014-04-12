9b7940cabfbec9ceba37e9461f69a4674d66ff5d
tholapz <tholapz@gmail.com>
2014-04-12 15:05:31 -0700

    include bio in team page

--- app/js/app.js
+++ app/js/app.js
@@ -2,15 +2,17 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+angular.module('authAnn', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+  'authAnn.filters',
+  'authAnn.services',
+  'authAnn.directives',
+  'authAnn.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'HomeController'});
+  $routeProvider.when('/team', {templateUrl: 'partials/team.html', controller: 'TeamController'});
+  $routeProvider.when('/contact', {templateUrl: 'partials/contact.html'});
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/home'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,36 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', [])
+angular.module('authAnn.controllers', [])
   .controller('MyCtrl1', [function() {
 
   }])
   .controller('MyCtrl2', [function() {
 
-  }]);
+  }])
+  .controller('HomeController', [function() {
+  	
+  }])
+  .controller('TeamController', function($scope) {
+  	$scope.members = [
+    {'name': 'Kamol Treewatchararat',
+     'photo': 'k.jpg',
+     'bio': "Kamol is a software developer with a masters degree from University of Southern California."},
+    {'name': 'Emma Chen',
+     'photo': 'emma.jpg',
+     'bio': 'Foodie. Violin player. Loves traveling. Loves cooking. Loves being a member in the Annenberg community!'},
+    {'name': 'Andre Domenech',
+     'photo': 'andre.jpg',
+     'bio':'Andres Domenech hails from Ann Arbor, Michigan, where he studied Spanish and Latin American Studies at the University of Michigan.'},
+    {'name': 'Jiyu Chen',
+     'photo': 'jiyu.jpg',
+     'bio':'Jiyu Chen is currently a second-year graduate student student in the Communication Management program.'},
+    {'name': 'Carol Zhu',
+     'photo':'carol.jpg',
+     'bio':'Carol Zhu is a second-year graduate student in the Communication Management program at the Annenberg School'},
+    {'name': 'Jan',
+     'photo': 'jan.png',
+     'bio': 'Jan Zari is a first year graduate student in the Master of' +
+     ' Communication Management program at the Annenberg School'}
+  ];
+  });
